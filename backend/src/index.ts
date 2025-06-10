9f69aec34402134f4cb0e03895df623e8376bb25
Chirag Makwana <makwanachirag427@gmail.com>
2025-06-10 21:09:10 +0530

    payload limit added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,9 +13,8 @@ import messageRoutes from "./routes/message.route";
 dotenv.config();
 const PORT = process.env.PORT || 5001;
 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
