9b279ed4df47bb35da1552aa9fd78c72acf42a00
Chirag Makwana <makwanachirag427@gmail.com>
2025-09-14 10:47:00 +0530

    minor fix

--- backend/src/config/token.ts
+++ backend/src/config/token.ts
@@ -2,16 +2,17 @@ import { Response } from "express";
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId: string, res: Response) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET!, {
-        expiresIn: "7d",
-    })
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET!, {
+    expiresIn: "7d",
+  });
 
-    res.cookie("jwt", token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== "development",
-        sameSite: "strict",
-    })
+  const isProd = process.env.NODE_ENV === "production";
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: isProd,
+    sameSite: isProd ? "none" : "lax",
+  });
 
-    return token;
-}
+  return token;
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db";
 import cors from "cors";
 import { app, server } from "./config/socket";
-import path from "path";
+// import path from "path";
 
 // routes
 import authRoutes from "./routes/auth.route";
@@ -12,13 +12,14 @@ import messageRoutes from "./routes/message.route";
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
+const frontendUrl = process.env.NODE_ENV === "development" ? process.env.CLIENT_URL : "https://talknow-backend.up.railway.app" 
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:frontendUrl,
     credentials: true,
   })
 );
@@ -26,13 +27,14 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+//   });
+// }
+
 server.listen(PORT, () => {
   connectDB();
   console.log(`App listening on port http://localhost:${PORT}`);
