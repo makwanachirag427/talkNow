2c4f68def99ff35dc9033f845978e828e985e7ce
Chirag Makwana <makwanachirag427@gmail.com>
2025-09-16 12:19:31 +0530

    cors error resolved

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,21 +12,19 @@ import messageRoutes from "./routes/message.route";
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
-const frontendUrl = process.env.NODE_ENV === "development" ? process.env.CLIENT_URL : "https://talknow-jbol.onrender.com" 
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin:frontendUrl,
+    origin: ["http://localhost:5173", "https://talknow-jbol.onrender.com"],
     credentials: true,
   })
 );
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
 
 server.listen(PORT, () => {
   connectDB();
