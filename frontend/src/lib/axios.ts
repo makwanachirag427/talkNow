ce27fdf0ae7b06fc4c318fa55d5f2081c6980816
Chirag Makwana <makwanachirag427@gmail.com>
2025-09-16 12:42:58 +0530

    backend url updated

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const  backendURL =  import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "talknow-production.up.railway.app/api";
+      : "https://talknow-production.up.railway.app/api";
 
 export const axiosInstance = axios.create({
   baseURL:
